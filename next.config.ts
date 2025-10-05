36e2890e7687764fd302a77301de3c5cfbfe563f
Ijay1234 <iijeoma360@gmail.com>
2025-10-05 20:38:18 +0100

    image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // 👈 replaces next export/* config options here */
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
